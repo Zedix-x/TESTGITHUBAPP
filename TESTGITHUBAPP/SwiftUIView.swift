ebaa7d6980dd464f82552ac07392820bcbc88797
J0KAIIR <alexis.dbw04@gmail.com>
2024-12-08 14:17:35 +0100

    House

---
+++ TESTGITHUBAPP/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  TESTGITHUBAPP
+//
+//  Created by apprenant95 on 08/12/2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
