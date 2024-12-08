b6892dbc940bd125c2150b3679560f7cd09ae17b
Zedix <oltamzao@gmail.com>
2024-12-08 13:24:36 +0100

    Initial Commit

---
+++ TESTGITHUBAPP/TESTGITHUBAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTGITHUBAPPApp.swift
+//  TESTGITHUBAPP
+//
+//  Created by apprenant110 on 08/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TESTGITHUBAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
